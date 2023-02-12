c797f795ab244637370e936a73cd02769abc225f
Saleh Alanazi <alanazi.saleh.77@gmail.com>
2023-02-12 17:37:27 +0300

    removed default where not neecessary

--- udagram/udagram-api/src/config/config.ts
+++ udagram/udagram-api/src/config/config.ts
@@ -1,11 +1,6 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 
-// ENV variables 
-// - AWS_ACCESS_KEY_ID
-// - AWS_SECRET_ACCESS_KEY
-// Are Also needed
-
 export const config = {
   username: `${process.env.POSTGRES_USERNAME}`,
   password: process.env.POSTGRES_PASSWORD,
@@ -14,6 +9,8 @@ export const config = {
   aws_region: process.env.AWS_REGION,
   aws_profile: process.env.AWS_PROFILE,
   aws_media_bucket: process.env.AWS_BUCKET,
+  AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+  AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
   url: process.env.URL,
   jwt: {
     secret: process.env.JWT_SECRET,
